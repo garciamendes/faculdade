de9c280d66e5621d0daa884cfd24cc7ffa82da5e
garciamendes <garciaprog0101@gmail.com>
2024-11-11 22:25:14 -0300

    Ajustes na dependence

--- pratica-integradora/i-teaching/src/main/java/com/gg/i_teaching/controller/InstructorController.java
+++ pratica-integradora/i-teaching/src/main/java/com/gg/i_teaching/controller/InstructorController.java
@@ -3,6 +3,7 @@ package com.gg.i_teaching.controller;
 import com.gg.i_teaching.dto.InstructorDTO;
 import com.gg.i_teaching.model.Instructor;
 import com.gg.i_teaching.service.InstructorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,10 @@ public class InstructorController {
     private InstructorService instructorService;
 
     @PostMapping
+    @Operation(summary="Criar um novo instrutor", description  = "Criação de estrutores")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "201", description = "A requisição foi executada com sucesso."),
+//            @ApiResponse(responseCode = "400", description = "Erro ao tentar criar.")})
     public ResponseEntity<Void> createInstructor(@RequestBody InstructorDTO data) {
         this.instructorService.create(data);
 
@@ -24,6 +29,10 @@ public class InstructorController {
     }
 
     @GetMapping("/{rg}")
+//    @Operation(summary="Pegar a instancia", description  = "Focado em pegar o detalhe do instrutor")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Retornando o instrutor."),
+//            @ApiResponse(responseCode = "404", description = "Não achado.")})
     public ResponseEntity<Optional<Instructor>> getFindInstructor(@PathVariable String rg) {
         Optional<Instructor> instructor = this.instructorService.getInstructor(rg);
 
